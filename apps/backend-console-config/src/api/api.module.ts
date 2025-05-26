e4469b9e5eee894d69fe418418cc5550ac2fcbb7
miyou <love@miyou.top>
2025-05-26 11:38:06 +0800

    feat(backend-console-config): init release

--- apps/backend-console-config/src/api/api.module.ts
+++ apps/backend-console-config/src/api/api.module.ts
@@ -5,10 +5,12 @@ import { VersionController } from './controller/version.controller';
 import { NamespaceService } from './service/namespace.service';
 import { ConfigurationService } from './service/configuration.service';
 import { VersionService } from './service/version.service';
+import { ReleaseController } from './controller/release.controller';
+import { ReleaseService } from './service/release.service';
 
 @Module({
-  controllers: [NamespaceController, ConfigurationController, VersionController],
-  providers: [NamespaceService, ConfigurationService, VersionService],
+  controllers: [NamespaceController, ConfigurationController, VersionController, ReleaseController],
+  providers: [NamespaceService, ConfigurationService, VersionService, ReleaseService],
   imports: [],
 })
 export class ApiModule {}
