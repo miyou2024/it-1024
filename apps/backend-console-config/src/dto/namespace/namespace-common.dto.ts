24f1fe7701541fd4979dd0f88fc4e0d83df2ccf7
miyou <love@miyou.top>
2025-05-26 14:10:32 +0800

    feat(backend-console-config): init common field

--- apps/backend-console-config/src/dto/namespace/namespace-common.dto.ts
+++ apps/backend-console-config/src/dto/namespace/namespace-common.dto.ts
@@ -1,1 +1,13 @@
-export class NamespaceCommonDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class NamespaceCommonDto {
+  @ApiProperty({
+    description: 'namespace name',
+  })
+  nsName: string;
+
+  @ApiProperty({
+    description: 'namespace code',
+  })
+  nsCode: string;
+}
